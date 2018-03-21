0ac54659dd684d81c84845e81f0424b672750c32
Vishal Nagda <vishalnagda1@gmail.com>
2018-03-21 23:21:38 +0530

    create a article model

--- article/models.py
+++ article/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, default='')
+    text = models.TextField(default='')
