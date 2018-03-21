ef4dcc99b01a9363cb62b00039f464d500a725d3
Vishal Nagda <vishalnagda1@gmail.com>
2018-03-22 01:04:43 +0530

    add new fields archive, created_at and updated_at in article model

--- article/models.py
+++ article/models.py
@@ -7,3 +7,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=200, default='')
     text = models.TextField(default='')
+    archive = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
