55b5168798d41ac5d3058d8e3e6e2a05abb9b626
Vishal Nagda <vishalnagda1@gmail.com>
2018-03-27 16:40:50 +0530

    create show api for article

--- article/views.py
+++ article/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -14,5 +15,24 @@ def index(request):
     articles = Article.objects.all()
     articles = ArticleSerializer(articles, many=True)
     return Response({
-        'articles': articles.data,
+        'articles': articles.data
     })
+
+
+@api_view(['GET'])
+def show(request):
+    try:
+        article_id = request.query_params['id']
+        article = Article.objects.get(id=article_id)
+        article = ArticleSerializer(article)
+        return Response({
+            'article': article.data
+        })
+    except ObjectDoesNotExist as e:
+        return Response({
+            'error': 'Article not found.',
+        }, status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        return Response({
+            'error': 'Invalid params passed',
+        }, status.HTTP_400_BAD_REQUEST)
