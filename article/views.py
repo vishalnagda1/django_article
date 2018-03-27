08bdab674206e71e5784cc1074840119e3c627aa
Vishal Nagda <vishalnagda1@gmail.com>
2018-03-27 15:21:31 +0530

    created a index api for article app

--- article/views.py
+++ article/views.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Article
+from .serializers import ArticleSerializer
 
 
+@api_view(['GET'])
 def index(request):
-    return HttpResponse('Hello Article')
+    articles = Article.objects.all()
+    articles = ArticleSerializer(articles, many=True)
+    return Response({
+        'articles': articles.data,
+    })
