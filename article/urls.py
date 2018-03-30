bede4450815c9b6f172c140ca1367481fe69a347
Vishal Nagda <vishalnagda1@gmail.com>
2018-03-30 18:51:49 +0530

    update urls for restful api call

--- article/urls.py
+++ article/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import url
 
-from . import views
+from article import views
+
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^show', views.show, name='show'),
-    url(r'^create', views.create, name='create'),
-    url(r'^update', views.update, name='update'),
-    url(r'^destroy', views.destroy, name='destroy'),
-    url(r'^unarchive', views.unarchive, name='unarchive'),
-    url(r'^archive', views.archive, name='archive'),
+    url(r'^$', views.articles, name='articles'),
+    url(r'^(?P<pk>[0-9]+)/$', views.article_ops),
 ]
