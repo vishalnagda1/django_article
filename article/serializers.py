6597107bf592737d8da4597f36ee9c32f44ffeee
Vishal Nagda <vishalnagda1@gmail.com>
2018-03-30 17:02:27 +0530

    update serializer to render all fields

--- article/serializers.py
+++ article/serializers.py
@@ -5,4 +5,4 @@ from models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('id', 'title', 'text', 'archive', 'created_at', 'updated_at')
+        fields = '__all__'
