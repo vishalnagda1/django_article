05d01884772ee2b461d5ee06eb2dcbbe18e603e2
Vishal Nagda <vishalnagda1@gmail.com>
2018-03-31 12:43:04 +0530

    update article's url

--- article/urls.py
+++ article/urls.py
@@ -5,5 +5,5 @@ from article import views
 
 urlpatterns = [
     url(r'^$', views.articles, name='articles'),
-    url(r'^(?P<pk>[0-9]+)/$', views.article_ops),
+    url(r'^(?P<pk>[0-9]+)/?$', views.article_ops),
 ]
